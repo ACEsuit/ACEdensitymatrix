4b1822dd586a34d02e5a04a0e3c5d6d7ea8080ea
zhanglw0521 <zhanglw@lsec.cc.ac.cn>
2024-04-10 16:17:40 +0200

    functions for constructing finally-used data

--- src/data_manupulation.jl
+++ src/data_manupulation.jl
@@ -1,2 +1,74 @@
 using DecoratedParticles
 
+include("../src/utils/hdf5.jl")
+include("../src/utils/reorder.jl")
+
+# translate a frame to a pair of data (R, D_R)
+function translate_frame(frame::Dict{String,Array})
+    Zs = Int64.(frame["Atomic numbers"]) 
+    Rs = [ frame["Coordinates"][:,i] for i in 1:size(frame["Coordinates"],2) ]
+    @assert length(Zs) == length(Rs)
+    R = [State(rr = Rs[j], Z = Zs[j]) for j in 1:length(Zs)]
+
+    C = copy(frame["Coefficients"]')
+    S = frame["Overlap"]
+    u, e, v = svd(S)
+    sqrt_S = u * diagm(sqrt.(e)) * v'
+    C = sqrt_S * C # Loewding transformation
+    C = apply_reorder(frame["Basis set labels"], C; debug=false) # Reorder the basis set
+    D = C * C' # Density matrix with correct ordering and on the manifold
+
+    return (R, D)
+end
+
+function get_state(R,I,J)
+    if I == J
+        # Onsite local environment
+        @assert I <= length(R)
+        return [State(rr = R[K].rr - R[I].rr, ZK = R[K].Z, ZI = R[I].Z) for K in setdiff(1:length(R), [I])]
+    else
+        # Offsite local environment - we have several options and here comes just one of them
+        # TODO: Implement the other options
+        @assert I <= length(R) && J <= length(R)
+        RIJ = [State(rr = R[K].rr - R[I].rr, rr0 = R[J].rr - R[I].rr, ZK = R[K].Z, ZI = R[I].Z, ZJ = R[J].Z, bond = false) for K in setdiff(1:length(R), [I,J])]
+        push!(RIJ, State(rr = R[J].rr - R[I].rr, rr0 = R[J].rr - R[I].rr, ZK = R[J].Z, ZI = R[I].Z, ZJ = R[J].Z, bond = true))
+    end
+end
+
+function get_block(D::Matrix{Float64},I::Int64,J::Int64,ao_labels::Vector{String})
+    # Get the block of the density matrix
+    # I,J are the indices of the atoms
+    # ao_labels are the labels of the basis set
+
+    # first assure that the ao_labels are in the correct order
+    ao_labels = apply_reorder(ao_labels)
+
+    atom_ids, atom_symbols, shells, ls, ms = unpack(ao_labels)
+    atom_ids .+= 1
+
+    pos_I = findall(x->x==I, atom_ids)
+    pos_J = findall(x->x==J, atom_ids)
+
+    return D[pos_I, pos_J]
+end
+
+function get_block(D::Matrix{Float64},I::Int64,J::Int64,ao_labels::Vector{String},L1::Int64,L2::Int64)
+    # Get the block of the density matrix
+    # I,J are the indices of the atoms
+    # L1,L2 are the angular momentum indices
+    # ao_labels are the labels of the basis set
+ 
+    # first assure that the ao_labels are in the correct order
+    ao_labels = apply_reorder(ao_labels)
+
+    atom_ids, atom_symbols, shells, ls, ms = unpack(ao_labels)
+    atom_ids .+= 1
+
+    pos_I = findall(x->x==I, atom_ids)
+    pos_J = findall(x->x==J, atom_ids)
+
+    pos_L1 = findall(x->x==L1, ls[pos_I])
+    pos_L2 = findall(x->x==L2, ls[pos_J])
+
+    return D[pos_I, pos_J][pos_L1, pos_L2]
+end
--- src/utils/reorder.jl
+++ src/utils/reorder.jl
@@ -1,10 +1,4 @@
-function apply_reorder(ao_labels::Vector{String}, matrix::Matrix{Float64};
-        inverse=false, debug=false)
-
-    # keep a copy of the reordering for debug
-    ref_order = Vector{Int64}(1:size(ao_labels, 1))
-
-    # unpack the labels
+function unpack(ao_labels::Vector{String})
     atom_ids = Int64[]
     atom_symbols = String[]
     shells = Int64[]
@@ -25,6 +19,19 @@ function apply_reorder(ao_labels::Vector{String}, matrix::Matrix{Float64};
         push!(ls, l)
         push!(ms, m)
     end
+
+    return atom_ids, atom_symbols, shells, ls, ms
+end
+
+
+function apply_reorder(ao_labels::Vector{String}, matrix::Matrix{Float64};
+        inverse=false, debug=false)
+
+    # keep a copy of the reordering for debug
+    ref_order = Vector{Int64}(1:size(ao_labels, 1))
+
+    # unpack the labels
+    atom_ids, atom_symbols, shells, ls, ms = unpack(ao_labels)
 
     rotated_matrix = copy(matrix)
 
@@ -92,4 +99,60 @@ function apply_reorder(ao_labels::Vector{String}, matrix::Matrix{Float64};
     end
 
     return rotated_matrix
+end
+
+function apply_reorder(ao_labels::Vector{String})
+    # keep a copy of the reordering for debug
+    ref_order = Vector{Int64}(1:size(ao_labels, 1))
+
+    # unpack the labels
+    atom_ids, atom_symbols, shells, ls, ms = unpack(ao_labels)
+
+    # apply the transformations atom by atom
+    for atom_id in 0:maximum(atom_ids)
+
+        # here we assume that functions on the same atom are contiguous,
+        # and compute the range of the atom block. If atoms are not
+        # contiguous this must be changes
+        atom_mask = findall(x->x==atom_id, atom_ids)
+        atom_start = minimum(atom_mask)
+        atom_stop = maximum(atom_mask)
+        atom_range = atom_start:atom_stop
+
+        # build and apply the unitary transformation for ordering m by value
+        for shell in 1:maximum(shells[atom_range])
+            shell_mask = findall(x->x==shell, shells[atom_range])
+            shell_start = minimum(shell_mask)
+            shell_stop = maximum(shell_mask)
+            shell_range = atom_start+shell_start-1:atom_start+shell_stop-1
+
+            # note: the range starts from 1 as we skip the s orbitals
+            for l in 1:maximum(ls[shell_range])
+                l_mask = findall(x->x==l, ls[shell_range])
+                l_start = minimum(l_mask)
+                l_stop = maximum(l_mask)
+                l_range = atom_start+shell_start+l_start-2:atom_start+shell_start+l_stop-2
+
+                Um = zeros(Float64, size(l_range, 1), size(l_range, 1))
+                m = ms[l_range]
+                for (i, j) in enumerate(sortperm(m))
+                    Um[i, j] = 1.0
+                end
+
+                ref_order[l_range] = Um * ref_order[l_range]
+            end
+        end
+
+        # build the unitary transformation for bringing the same values
+        # of l close
+        Ul = zeros(Float64, size(atom_range, 1), size(atom_range, 1))
+        atom_ls = ls[atom_range]
+        for (i, j) in enumerate(sortperm(atom_ls))
+            Ul[i, j] = 1.0
+        end
+
+        ref_order[atom_range] = Ul * ref_order[atom_range]
+    end
+
+    return ao_labels[ref_order]
 end
