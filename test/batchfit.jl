8834d303032b2eedee18d4c3fcba8eb20a62d1aa
zhanglw0521 <zhanglw@lsec.cc.ac.cn>
2024-04-30 12:10:04 +0200

    clean up

--- test/batchfit.jl
+++ test/batchfit.jl
@@ -1,9 +1,10 @@
-using Statistics, Plots, PythonCall
+using Statistics, PythonCall, JLD # , Plots
 using ArgParse
 
 include("../src/data_manupulation.jl")
 include("../src/model_construction.jl")
 include("../src/fit.jl")
+include("../src/io.jl")
 
 
 args = ArgParseSettings()
@@ -33,7 +34,7 @@ end
 parsed_args = parse_args(ARGS, args)
 
 # read data 
-filenames = ["../data/propanol.h5", "../data/esanol.h5", "../data/acrolein.h5", "../data/phenol.h5", "../data/toluene.h5", "../data/acetaldehyde.h5"]
+filenames = ["../data/propanol.h5", "../data/esanol.h5", "../data/acrolein.h5", "../data/phenol.h5", "../data/toluene.h5", "../data/acetaldehyde.h5", "../data/aniline.h5", "../data/nmacetamide.h5"]
 Ndata = parsed_args["ndata"] # number of training data we use for a single molecule
 frames = []
 for fname in filenames
@@ -42,9 +43,17 @@ for fname in filenames
 end
 frames = identity.(frames)
 
+frames_test = []
+for fname in filenames
+    molecule = TrajectoryHDF5(fname)
+    push!(frames_test,[ read_frame(molecule,Int(i)) for i =2:floor(10000/Ndata):10000 ]...) # constructing a test data set with Ndata frames for a single .h5 file
+end
+frames_test = identity.(frames_test)
+
 # construct a model 
-ao_dict = Dict( 6 => Dict("n_orbs" => [3,2,1], "maxdeg" => parsed_args["degree"], "ord" => parsed_args["order"], "rcut" => parsed_args["rcut"], "zcut" => parsed_args["zcut"]), 
-                1 => Dict("n_orbs" => [2], "maxdeg" => parsed_args["degree"], "ord" => parsed_args["order"], "rcut" => parsed_args["rcut"], "zcut" => parsed_args["zcut"]), 
+ao_dict = Dict( 1 => Dict("n_orbs" => [2], "maxdeg" => parsed_args["degree"], "ord" => parsed_args["order"], "rcut" => parsed_args["rcut"], "zcut" => parsed_args["zcut"]), 
+                6 => Dict("n_orbs" => [3,2,1], "maxdeg" => parsed_args["degree"], "ord" => parsed_args["order"], "rcut" => parsed_args["rcut"], "zcut" => parsed_args["zcut"]), 
+                7 => Dict("n_orbs" => [3,2,1], "maxdeg" => parsed_args["degree"], "ord" => parsed_args["order"], "rcut" => parsed_args["rcut"], "zcut" => parsed_args["zcut"]),
                 8 => Dict("n_orbs" => [3,2,1], "maxdeg" => parsed_args["degree"], "ord" => parsed_args["order"], "rcut" => parsed_args["rcut"], "zcut" => parsed_args["zcut"]) )
 
 println("Constructing the model ...")
@@ -58,29 +67,50 @@ println()
 # fit the model
 fit!(DM, frames; solver = ACEfit.SKLEARN_BRR())
 
-# validate the model
-fname = rand(filenames)
-molecule = TrajectoryHDF5(fname)
-frame = read_frame(molecule,rand(1:10000))
-R, D = translate_frame(frame)["R"], translate_frame(frame)["D"] # read a random frame
-@time D_pred = eval_model(DM, R, translate_frame(frame)["ao_labels"]) # predicted density matrix
-
-# error in RMSE && relative error in D
-E = norm(D_pred - D)/sqrt(size(D,1)*size(D,2))
-println("Test RMSE = $E")
+# validate the model - Training
+RE = 0
+RMSE = 0
+MV = 0
+for frame in frames
+    R, D = translate_frame(frame)["R"], translate_frame(frame)["D"]
+    D_pred = eval_model(DM, R, translate_frame(frame)["ao_labels"]) # predicted density matrix
+    RMSE += norm(D_pred - D)^2/(size(D,1)*size(D,2))
+    RE += norm(D_pred - D)/norm(D)
+    MV += norm(D_pred * D_pred - D_pred)
+end
+println("Training RMSE per matrix element = $(sqrt(RMSE/length(frames)))")
+println("Average training relative error in D: ||D - D_ref|| / ||D||= $(RE/length(frames))")
+println("Average training manifold violation = $(MV/length(frames))")
 println()
-E = norm(D_pred - D)/norm(D)
-println("Relative error in D ||D - D_ref|| / ||D||= $E")
+
+# validate the model - Test
+RE = 0
+RMSE = 0
+MV = 0
+for frame in frames_test
+    R, D = translate_frame(frame)["R"], translate_frame(frame)["D"]
+    D_pred = eval_model(DM, R, translate_frame(frame)["ao_labels"]) # predicted density matrix
+    RMSE += norm(D_pred - D)^2/(size(D,1)*size(D,2))
+    RE += norm(D_pred - D)/norm(D)
+    MV += norm(D_pred * D_pred - D_pred)
+end
+println("Test RMSE per matrix element = $(sqrt(RMSE/length(frames_test)))")
+println("Average test relative error in D: ||D - D_ref|| / ||D||= $(RE/length(frames_test))")
+println("Average test manifold violation = $(MV/length(frames_test))")
 println()
 
-# visualize the error
-E = abs.(D_pred - D)
-plt = contourf(E);
-savefig("Error_Degree$(parsed_args["degree"])_Ord$(parsed_args["order"])")
+# # visualize the error
+# E = abs.(D_pred - D)
+# plt = contourf(E);
+# savefig("Error_Degree$(parsed_args["degree"])_Ord$(parsed_args["order"])")
 
-# Manifold violation
-# norm(D * D - D)
-println("Manifold violation = $(norm(D_pred * D_pred - D_pred))")
-# @assert norm(D_pred * D_pred - D_pred) ≤ (2*norm(D)+2)*norm(E) # an extremely rough error bound
+# save the model
+println("Saving the model ...")
+println()
+
+save("CHON_Models/model_maxdeg$(parsed_args["degree"])_ord$(parsed_args["order"])_rcut$(parsed_args["rcut"])_zcut$(parsed_args["zcut"]).jld",  DM)
+
+println("Model saved!")
+println()
 
 println("Done.")
--- test/main.jl
+++ test/main.jl
@@ -1,12 +1,18 @@
-using Statistics, Plots
+using Statistics, PythonCall, JLD # , Plots
 
 include("../src/data_manupulation.jl")
 include("../src/model_construction.jl")
 include("../src/fit.jl")
+include("../src/io.jl")
+
+Ndata = 1
+rcut = 10.0
+zcut = 10.0
+degree = 6
+order = 2
 
 # read data 
-filenames = ["data/propanol.h5"]#, "data/esanol.h5", "data/acrolein.h5", "data/phenol.h5", "data/toluene.h5", "data/acetaldehyde.h5"]#, "data/aniline.h5", "data/nmacetamide.h5"]
-Ndata = 120 # number of training data we use for a single molecule
+filenames = ["data/propanol.h5"]#, "data/esanol.h5", "data/acrolein.h5", "data/phenol.h5", "data/toluene.h5", "data/acetaldehyde.h5", "data/aniline.h5", "data/nmacetamide.h5"]
 frames = []
 for fname in filenames
     molecule = TrajectoryHDF5(fname)
@@ -14,43 +20,81 @@ for fname in filenames
 end
 frames = identity.(frames)
 
+frames_test = []
+for fname in filenames
+    molecule = TrajectoryHDF5(fname)
+    push!(frames_test,[ read_frame(molecule,Int(i)) for i =2:floor(10000/Ndata):10000 ]...) # constructing a test data set with Ndata frames for a single .h5 file
+end
+frames_test = identity.(frames_test)
+
 # construct a model 
-ao_dict = Dict( 6 => Dict("n_orbs" => [3,2,1], "maxdeg" => 6, "ord" => 1, "rcut" => 10.0, "zcut" => 10.0), 
-                1 => Dict("n_orbs" => [2], "maxdeg" => 6, "ord" => 1, "rcut" => 10.0, "zcut" => 10.0),
-                # 7 => Dict("n_orbs" => [3,2,1], "maxdeg" => 4, "ord" => 3, "rcut" => 10.0, "zcut" => 10.0), 
-                8 => Dict("n_orbs" => [3,2,1], "maxdeg" => 6, "ord" => 1, "rcut" => 10.0, "zcut" => 10.0) )
+ao_dict = Dict( 1 => Dict("n_orbs" => [2], "maxdeg" => degree, "ord" => order, "rcut" => rcut, "zcut" => zcut), 
+                6 => Dict("n_orbs" => [3,2,1], "maxdeg" => degree, "ord" => order, "rcut" => rcut, "zcut" => zcut),
+                7 => Dict("n_orbs" => [3,2,1], "maxdeg" => degree, "ord" => order, "rcut" => rcut, "zcut" => zcut),
+                8 => Dict("n_orbs" => [3,2,1], "maxdeg" => degree, "ord" => order, "rcut" => rcut, "zcut" => zcut) )
+
+println("Constructing the model ...")
+println()
 
 DM = Density_Model(ao_dict::Dict) # a density matrix model corresponding to the atomic orbital dictionary
 
+println("Model constructed!")
+println()
+
 # fit the model
-fit!(DM, frames)#; solver = ACEfit.QR(lambda = 1e-12, P = I))
+fit!(DM, frames; solver = ACEfit.SKLEARN_BRR())
 
-# evaluate the model
-# convert / something else <- translate
-R, D = translate_frame(frames[1])["R"], translate_frame(frames[1])["D"]
-D_pred = @time eval_model(DM, R, translate_frame(frames[1])["ao_labels"]) # predicted density matrix
-R, D = translate_frame(frames[3])["R"], translate_frame(frames[3])["D"]
-D_pred = @time eval_model(DM, R, translate_frame(frames[3])["ao_labels"]) # predicted density matrix
-
-# maximum error
-D_pred - D |> maximum
-
-# visualize the error
-E = abs.(D_pred - D)
-norm(E) / norm(D) # relative error
-contourf(E)
-contourf(D)
-
+# validate the model - Training
+RE = 0
 RMSE = 0
+MV = 0
 for frame in frames
     R, D = translate_frame(frame)["R"], translate_frame(frame)["D"]
     D_pred = eval_model(DM, R, translate_frame(frame)["ao_labels"]) # predicted density matrix
-    @time eval_model(DM, R, translate_frame(frame)["ao_labels"]) # predicted density matrix
-    RMSE += norm(D_pred - D)^2 / size(D,1) / size(D,2) # RMSE error for a single frame - scaled
+    RMSE += norm(D_pred - D)^2/(size(D,1)*size(D,2))
+    RE += norm(D_pred - D)/norm(D)
+    MV += norm(D_pred * D_pred - D_pred)
 end
-RMSE = RMSE / length(frames) |> sqrt # RMSE error for the training set
+println("Training RMSE per matrix element = $(sqrt(RMSE/length(frames)))")
+println("Average training relative error in D: ||D - D_ref|| / ||D||= $(RE/length(frames))")
+println("Average training manifold violation = $(MV/length(frames))")
+println()
 
-# Manifold violation
-norm(D * D - D)
-norm(D_pred * D_pred - D_pred)
-@assert norm(D_pred * D_pred - D_pred) ≤ (2*norm(D)+2)*norm(E) # an extremely rough error bound
+# validate the model - Test
+RE = 0
+RMSE = 0
+MV = 0
+for frame in frames_test
+    R, D = translate_frame(frame)["R"], translate_frame(frame)["D"]
+    D_pred = eval_model(DM, R, translate_frame(frame)["ao_labels"]) # predicted density matrix
+    RMSE += norm(D_pred - D)^2/(size(D,1)*size(D,2))
+    RE += norm(D_pred - D)/norm(D)
+    MV += norm(D_pred * D_pred - D_pred)
+end
+println("Test RMSE per matrix element = $(sqrt(RMSE/length(frames_test)))")
+println("Average test relative error in D: ||D - D_ref|| / ||D||= $(RE/length(frames_test))")
+println("Average test manifold violation = $(MV/length(frames_test))")
+println()
+
+# # visualize the error
+# E = abs.(D_pred - D)
+# plt = contourf(E);
+# savefig("Error_Degree$(parsed_args["degree"])_Ord$(parsed_args["order"])")
+
+# save the model
+println("Saving the model ...")
+println()
+
+save("test/CHON_Models/model_maxdeg$(degree)_ord$(order)_rcut$(rcut)_zcut$(zcut).jld", write_dict(DM))
+
+
+println("Model saved!")
+println()
+
+println("Done.")
+
+# Backup: codes for checking the saved and loaded model are the same (at least do the same thing)
+# dm = read_dict(load("model_deg$(maxdeg)_ord$(ord)_rcut$(rcut)_zcut$(zcut).jld"))
+# @time eval_model(dm, R, translate_frame(frames[1])["ao_labels"]) # predicted density matrix
+# @time eval_model(DM, R, translate_frame(frames[1])["ao_labels"]) # predicted density matrix
+# @show eval_model(dm, R, translate_frame(frames[1])["ao_labels"]) == eval_model(DM, R, translate_frame(frames[1])["ao_labels"])
