262878a0d2f1eed2138a1dbab8e320225710dc5e
zhanglw0521 <zhanglw@lsec.cc.ac.cn>
2024-05-16 11:29:00 +0200

    polish io_tests

--- test/io_tests.jl
+++ test/io_tests.jl
@@ -21,8 +21,10 @@ DM2 = Density_Model(ao_dict_2) # a density matrix model corresponding to the ato
 Zi = rand(keys(ao_dict_1))::Int
 Zj = rand(keys(ao_dict_1))::Int
 Zi, Zj = min(Zi,Zj), max(Zi,Zj)
-onsite_model = DM1.Models[Zi]
-offsite_model = DM1.Models[Zi, Zj]
+onsite_model1 = DM1.Models[Zi]
+offsite_model1 = DM1.Models[Zi, Zj]
+onsite_model2 = DM2.Models[Zi]
+offsite_model2 = DM2.Models[Zi, Zj]
 
 # construct random configurations
 
@@ -34,24 +36,40 @@ Roff = begin
 end
 
 # test for IO - onsite radial basis 
-md = onsite_model.model
+md = onsite_model1.model
+l = md.layers.embed.layers.Rn
+@show read_dict(write_dict(l)) == l
+
+md = onsite_model2.model
 l = md.layers.embed.layers.Rn
 @show read_dict(write_dict(l)) == l
 
 # test for IO - offsite radial basis
-md = offsite_model.model
+md = offsite_model1.model
+l = md.layers.embed.layers.Rn
+@show read_dict(write_dict(l)) == l
+
+md = offsite_model2.model
 l = md.layers.embed.layers.Rn
 @show read_dict(write_dict(l)) == l
 
 # test for IO - onsite model
-d = write_dict(onsite_model)
+d = write_dict(onsite_model1)
 onmd = read_dict(d)
-eval_model(onmd, Ron) == eval_model(onsite_model, Ron)
+eval_model(onmd, Ron) == eval_model(onsite_model1, Ron)
+
+d = write_dict(onsite_model2)
+onmd = read_dict(d)
+eval_model(onmd, Ron) == eval_model(onsite_model2, Ron)
 
 # test for IO - offsite model
-d = write_dict(offsite_model)
+d = write_dict(offsite_model1)
 offmd = read_dict(d)
-eval_model(offmd, Roff) == eval_model(offsite_model, Roff)
+eval_model(offmd, Roff) == eval_model(offsite_model1, Roff)
+
+d = write_dict(offsite_model2)
+offmd = read_dict(d)
+eval_model(offmd, Roff) == eval_model(offsite_model2, Roff)
 
 # test for IO - Density_Model
 # Here we need a example data
@@ -59,15 +77,27 @@ molecule = TrajectoryHDF5("data/propanol.h5")
 frame = read_frame(molecule,rand(1:10000))
 R, D, ao_labels = translate_frame(frame)["R"], translate_frame(frame)["D"], translate_frame(frame)["ao_labels"]
 
-d = write_dict(DM)
+d = write_dict(DM1)
 dm = read_dict(d)
 @time eval_model(dm, R, ao_labels);
-@time eval_model(DM, R, ao_labels);
-@show eval_model(dm, R, ao_labels) == eval_model(DM, R, ao_labels)
+@time eval_model(DM1, R, ao_labels);
+@show eval_model(dm, R, ao_labels) == eval_model(DM1, R, ao_labels)
+
+d2 = write_dict(DM2)
+dm = read_dict(d2)
+@time eval_model(dm, R, ao_labels);
+@time eval_model(DM2, R, ao_labels);
+@show eval_model(dm, R, ao_labels) == eval_model(DM2, R, ao_labels)
 
 # test for IO - save and load to a local file
 save("test/model.jld", d)
 dm = load("test/model.jld") |> read_dict
 @time eval_model(dm, R, ao_labels);
-@time eval_model(DM, R, ao_labels);
-@show eval_model(dm, R, ao_labels) == eval_model(DM, R, ao_labels)
+@time eval_model(DM1, R, ao_labels);
+@show eval_model(dm, R, ao_labels) == eval_model(DM1, R, ao_labels)
+
+save("test/model.jld", d2)
+dm = load("test/model.jld") |> read_dict
+@time eval_model(dm, R, ao_labels);
+@time eval_model(DM2, R, ao_labels);
+@show eval_model(dm, R, ao_labels) == eval_model(DM2, R, ao_labels)
